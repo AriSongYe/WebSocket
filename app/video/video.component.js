056e42f7042318b79fd2ca7dd3312999d2846fcd
AriSongYe <kow14671@gmail.com>
2024-06-12 11:07:45 +0900

    Movie API Complete

--- app/video/video.component.js
+++ app/video/video.component.js
@@ -6,16 +6,14 @@ angular.module('video')
     controller: ['$http', function videoController($http){
       var self = this;
       self.videoUrl = 'https://yts.mx/api/v2/list_movies.json';
-      self.user = 'yechan';
-      console.log('Movie component Test');
 
       $http.get(self.videoUrl)
         .then(function(response) {
           console.log('Movie details fetched:', response.data);  // 데이터 로그 출력
-          self.movieDetails = response.data;
+          self.movieList = response.data;
         })
         .catch(function(error) {
           console.error('Error get movie details:', error);
         })
     }]
-  })
+  }) 
